0ffbb68a5404d86476f99adac7cf4a8d08623f6f
Victor <borismustanger@gmail.com>
2023-08-08 18:08:48 +0800

    name

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
                 {/* Overlay */}
                 <div className="absolute w-full h-full text-gray-200 max-h-[600px] bg-black/40 flex flex-col justify-center">
                     <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">Самая <span className="text-orange-600">Лучшая</span></h1>
-                    <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold"> <span className="text-orange-600">Еда</span> Здесь</h1>
+                    <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold"> <span className="text-orange-600">Еда</span> еDalivery</h1>
                 </div>
                 <img className="w-full max-h-[600px] object-cover" src="https://images.rawpixel.com/image_1000/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvZmYyNjIyLWltYWdlLWt3dnluZXd0LmpwZw.jpg" alt="/" />
             </div>
