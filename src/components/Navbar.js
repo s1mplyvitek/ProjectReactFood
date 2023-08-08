e2437a851cb5ee07431ddf9c6f7913bc1de875a5
Victor <borismustanger@gmail.com>
2023-08-08 18:19:15 +0800

    nameproject

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
                     <AiOutlineMenu size={30} />
                 </div>
                 <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
-                    Лучшая <span className="font-bold">Еда</span>
+                   <span className="font-bold">еDa</span>livery
                 </h1>
                 <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
                     <p className="bg-black text-white rounded-full p-2">Доставка</p>
