38cc88e13b3d5fbd087304a1134995baa370b49d
Victor <borismustanger@gmail.com>
2023-08-21 21:02:40 +0800

    footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 
 import React from "react";
-
 import Category from "./components/Category";
 import { Outlet } from "react-router-dom";
 import Navunder from "./components/Navunder";
+import Footer from "./components/Footer";
+
 
 
 function App() {
   return (
     <div>
-      <Navunder />
-      {/* <Navbar /> */}
+      <Navunder />      
       <Outlet></Outlet>
-      <Category />      
+      <Category /> 
+      <Footer />         
     </div>
 
 
