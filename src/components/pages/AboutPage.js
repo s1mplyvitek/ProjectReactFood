81c657cbd0e842d155861e7eb829207216bd08d3
Victor <borismustanger@gmail.com>
2023-09-07 19:55:12 +0800

    test

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const AboutPage = () => {
     return (
         <>
-            <div className="w-2/3 mx-auto">
+            <div className="w-2/3 mx-auto grid">
                 Мы скоро откроемся!
                 <Link to={"contacts"}>
                     <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
